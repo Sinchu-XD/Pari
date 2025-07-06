865afa3b7b790c3f83cb3a241535bd96d325ce68
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-07-06 11:03:45 +0530

    Update start.py

--- MoonXMusic/plugins/bot/start.py
+++ MoonXMusic/plugins/bot/start.py
@@ -1,5 +1,4 @@
 import time
-
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
@@ -24,6 +23,14 @@ from config import BANNED_USERS
 from strings import get_string
 
 
+# ✅ Util: Keyboard Sanitizer
+def sanitize_keyboard(keyboard):
+    return [
+        [btn for btn in row if btn is not None]
+        for row in keyboard if row is not None and any(btn is not None for btn in row)
+    ]
+
+
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
@@ -31,11 +38,11 @@ async def start_pm(client, message: Message, _):
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
-            keyboard = help_pannel(_)
+            keyboard = sanitize_keyboard(help_pannel(_))
             return await message.reply_photo(
                 photo=config.START_IMG_URL,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
-                reply_markup=keyboard,
+                reply_markup=InlineKeyboardMarkup(keyboard),
             )
         if name[0:3] == "sud":
             await sudoers_list(client=client, message=message, _=_)
@@ -63,12 +70,12 @@ async def start_pm(client, message: Message, _):
                 title, duration, views, published, channellink, channel, app.mention
             )
             key = InlineKeyboardMarkup(
-                [
+                sanitize_keyboard([
                     [
                         InlineKeyboardButton(text=_["S_B_8"], url=link),
                         InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
                     ],
-                ]
+                ])
             )
             await m.delete()
             await app.send_photo(
@@ -83,7 +90,7 @@ async def start_pm(client, message: Message, _):
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
     else:
-        out = private_panel(_)
+        out = sanitize_keyboard(private_panel(_))
         await message.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
@@ -99,7 +106,7 @@ async def start_pm(client, message: Message, _):
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def start_gp(client, message: Message, _):
-    out = start_panel(_)
+    out = sanitize_keyboard(start_panel(_))
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
         photo=config.START_IMG_URL,
@@ -135,7 +142,7 @@ async def welcome(client, message: Message):
                     )
                     return await app.leave_chat(message.chat.id)
 
-                out = start_panel(_)
+                out = sanitize_keyboard(start_panel(_))
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_3"].format(
